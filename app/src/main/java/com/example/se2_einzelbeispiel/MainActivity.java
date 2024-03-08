249c6b39fa5952321f8ecab488631580c78d7d0e
Netrunner <lujordan@edu.aau.at>
2024-03-08 10:43:14 +0100

    Created UI and implemented functionality for 2.2

--- app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.se2_einzelbeispiel;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,13 +18,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.nio.Buffer;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView serverResponse;
     private EditText mNr;
     private Button sendButton;
+    private Button calculateButton;
+    private TextView calculationResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +41,20 @@ public class MainActivity extends AppCompatActivity {
         serverResponse = findViewById(R.id.server_response);
         mNr = findViewById(R.id.mNr);
         sendButton = findViewById(R.id.send_button);
+        calculateButton = findViewById(R.id.calculate_button);
+        calculationResponse = findViewById(R.id.calculation_response);
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 connectToServer(view);
+            }
+        });
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                commonDivisorPairs(view);
             }
         });
     }
@@ -81,5 +92,34 @@ public class MainActivity extends AppCompatActivity {
             }
         }).start();
 
+    }
+
+    public void commonDivisorPairs(View view){
+        String number = mNr.getText().toString();
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 0; i < number.length(); i++){
+            for(int j = i + 1; j < number.length(); j++) {
+                int digit1 = Character.getNumericValue(number.charAt(i));
+                int digit2 = Character.getNumericValue(number.charAt(j));
+                Log.d("commonDivisorPairs", "Comparing indices" + i + " and " + j + ": " + digit1 + " & " + digit2);
+                if (commonDivisor(digit1, digit2)){
+                    result.append(i).append(" - ").append(j).append("\n");
+                    Log.d("commonDivisorPairs", "Found CD indices: " + i + " - " + j);
+                }
+
+            }
+        }
+        String concatenate = "Indices: \n" + result; // because of warning from setText
+        calculationResponse.setText(concatenate);
+    }
+
+    private boolean commonDivisor(int a, int b){
+        for (int d = 2; d <= Math.min(a, b); d++) {
+            if (a % d == 0 && b % d== 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
